bc794571609e8b4c38ec49f6c95a1f28be6edf2b
exeeny <your-email@example.com>
2025-05-30 23:06:49 +0400

    gmail

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -157,7 +157,7 @@ class ResetPasswordController extends AbstractController
         }
 
         $email = (new TemplatedEmail())
-            ->from(new Address('mailer@demomailtrap.com', 'mail bot'))
+            ->from(new Address('mirovagena1313@gmail.com', 'mail bot'))
             ->to((string) $user->getEmail())
             ->subject('Your password reset request')
             ->htmlTemplate('reset_password/email.html.twig')
